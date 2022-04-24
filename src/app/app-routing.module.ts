510299b9df738e1d39ddd0f44165c4e01a4498f0
karim369 <karim.chakroun@outlook.com>
2022-04-24 02:10:07 +0200

    commit11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 import { HomeComponent } from './home/home.component';
+import { InvitationsComponent } from './invitations/invitations.component';
 import { PostComponent } from './post/post.component';
 import { ProfilComponent } from './profil/profil.component';
 import { TravelComponent } from './travel/travel.component';
@@ -19,7 +21,9 @@ const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'profil',component:ProfilComponent},
   {path:'post',component:PostComponent},
-  {path:'travel',component:TravelComponent}
+  {path:'travel',component:TravelComponent},
+  {path:"changePassword/:id", component:ChangePasswordComponent},
+  {path:'invitations',component:InvitationsComponent},
 ];
 
 @NgModule({
--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ImageService } from '../shared/image.service';
 import { UserService } from '../shared/user.service';
 
 @Component({
@@ -9,7 +11,13 @@ import { UserService } from '../shared/user.service';
 })
 export class ProfilComponent implements OnInit {
 
-  constructor(private router:Router,private service:UserService) { }
+  constructor(private router:Router,private service:UserService,private httpClient: HttpClient,private imageService:ImageService) { }
+
+  dbImage: any; 
+  postResponse: any;
+  successResponse: string;
+  image: any;
+  uploadedImage: File; 
 
   userDetails:any;
 
@@ -17,13 +25,54 @@ export class ProfilComponent implements OnInit {
 
     this.service.getUserProfile().subscribe(
       res =>{
-        this.userDetails = res;
+        this.userDetails = res,
+        this.imageService.getImage(this.userDetails.imageName).subscribe(
+          res =>{
+            this.postResponse = res;          
+                this.dbImage = 'data:image/jpeg;base64,' + this.postResponse.image;
+          }
+    
+        );
       },
       err =>{
         console.log(err);
       }
 
     );
+
+    
   }
+
+  public onImageUpload(event) {    
+    this.uploadedImage = event.target.files[0];
+  }
+
+  imageUploadAction() {    
+    const imageFormData = new FormData();
+    imageFormData.append('image', this.uploadedImage, this.uploadedImage.name);
+  
+
+    this.httpClient.post('http://localhost:8089/SpringMVC/upload/image/' + this.userDetails.idClient , imageFormData, { observe: 'response' })
+      .subscribe((response) => {
+        if (response.status === 200) { 
+          this.postResponse = response;                
+          this.successResponse = this.postResponse.body.message;
+        } else {
+          this.successResponse = 'Image not uploaded due to some error!';
+        }
+      }
+      );
+    }
+
+    viewImage() {
+      this.httpClient.get('http://localhost:8089/SpringMVC/get/image/info/' + this.userDetails.imageName)
+        .subscribe(
+          res => {
+            this.postResponse = res;          
+           // this.dbImage = 'data:image/jpeg;base64,' + this.postResponse.image;
+           return 'data:image/jpeg;base64,' + this.postResponse.image;
+          }
+        );
+    }
 
 }
